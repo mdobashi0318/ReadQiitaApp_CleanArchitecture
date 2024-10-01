61b725294912b3d8dd94ca8a7db727e63ab4b8e2
MasaharuDobashi <dobashi.masaharu@gmail.com>
2024-10-01 23:19:22 +0900

    Initial Commit

---
+++ ReadQiitaApp_CleanArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadQiitaApp_CleanArchitecture
+//
+//  Created by 土橋正晴 on 2024/10/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ReadQiitaApp_CleanArchitectureTests/ReadQiitaApp_CleanArchitectureTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReadQiitaApp_CleanArchitectureTests.swift
+//  ReadQiitaApp_CleanArchitectureTests
+//
+//  Created by 土橋正晴 on 2024/10/01.
+//
+
+import Testing
+@testable import ReadQiitaApp_CleanArchitecture
+
+struct ReadQiitaApp_CleanArchitectureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
